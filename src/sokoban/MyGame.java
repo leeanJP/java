9ba75a1959541eb4962928587d5326d876d23dae
leean <zzzttt9@gmail.com>
2023-11-23 14:16:49 +0900

    Sokoban Update

--- src/sokoban/MyGame.java
+++ src/sokoban/MyGame.java
@@ -30,6 +30,7 @@ class MySokoban extends JFrame implements KeyListener, ActionListener{
     Image Dot = Toolkit.getDefaultToolkit().getImage("src/sokoban/resources/Dot.png");
     Image Box = Toolkit.getDefaultToolkit().getImage("src/sokoban/resources/Box.png");
     Image Road = Toolkit.getDefaultToolkit().getImage("src/sokoban/resources/Road.png");
+    Image Man = ManF;
 
     int stage = 0;
     int iXMan = 0;
@@ -49,42 +50,228 @@ class MySokoban extends JFrame implements KeyListener, ActionListener{
 
     String [][] Stage = {
             //stage1
-            {"  ####",
-             "  #  ###",
-             "  #    #",
-             "  # $  #",
-             "### ###",
-             "# $ $ #",
-             "#..@..#",
-             "#  $  #",
-             "###  ##",
-             "####",
+            {"  ####        ",
+             "  #  ###      ",
+             "  #    #      ",
+             "  # $  #      ",
+             "### ###       ",
+             "# $ $ #       ",
+             "#..@..#       ",
+             "#  $  #       ",
+             "###  ##       ",
+             "####          ",
             },
             //stage2
-            {"#####",
-             "#  @####",
-             "#  $.  #",
-             "###$.# #",
-             "#  $.# #",
-             "# #$.  #",
-             "#    ###",
-             "######",
+            {"##############",
+             "#  @$.       #",
+             "#   $ ###    #",
+             "#   . ###    #",
+             "#            #",
+             "#   ####  #  #",
+             "#   ####  #  #",
+             "#   #     #  #",
+             "#            #",
+             "##############"
             },
-
     };
 
+    public MySokoban() {
+        this.setTitle(TITLE);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container c = this.getContentPane();
+        c.setLayout(null);
+
+        btnUP = new JButton("UP");
+        btnUP.setFont(new Font("Arial", Font.BOLD, 15));
+        btnUP.setLocation(890, 290);
+        btnUP.setSize(100, 50);
+        btnUP.addActionListener(this);
+        c.add(btnUP);
+
+        btnDOWN = new JButton("DOWN");
+        btnDOWN.setFont(new Font("Arial", Font.BOLD, 15));
+        btnDOWN.setLocation(890, 390);
+        btnDOWN.setSize(100, 50);
+        btnDOWN.addActionListener(this);
+        c.add(btnDOWN);
+
+        btnLEFT = new JButton("LEFT");
+        btnLEFT.setFont(new Font("Arial", Font.BOLD, 15));
+        btnLEFT.setLocation(850, 340);
+        btnLEFT.setSize(100, 50);
+        btnLEFT.addActionListener(this);
+        c.add(btnLEFT);
+
+        btnRIGHT = new JButton("RIGHT");
+        btnRIGHT.setFont(new Font("Arial", Font.BOLD, 15));
+        btnRIGHT.setLocation(940, 340);
+        btnRIGHT.setSize(100, 50);
+        btnRIGHT.addActionListener(this);
+        c.add(btnRIGHT);
+
+        btnReset = new JButton("RESET");
+        btnReset.setFont(new Font("Arial", Font.BOLD, 15));
+        btnReset.setLocation(890, 150);
+        btnReset.setSize(100, 50);
+        btnReset.addActionListener(this);
+        c.add(btnReset);
+
+        TitleLabel = new JLabel();
+        TitleLabel.setFont(new Font("굴림", Font.BOLD, 25));
+        TitleLabel.setLocation(880, 50);
+        TitleLabel.setSize(200,50);
+        c.add(TitleLabel);
+
+        c.addKeyListener(this);
+        c.setFocusable(true);
+        c.requestFocus();
+
+        this.setSize(DXSize+MXSize+200, DYSize+MYSize);
+        this.setResizable(false);
+        this.setVisible(true);
+
+        LoadMap();
+        TitleLabel.setText("Score : " + iScore);
+
+    }
+
+    void LoadMap(){
+        for(int i=0; i<Stage[stage].length; ++i){
+            Map[i] = Stage[stage][i].toCharArray();
+        }
+        Man = ManF;
+        iScore = 0;
+    }
+
+    public void paint(Graphics g){
+        Image aImage;
+
+        bEndGame = true;
+        super.paintComponents(g);
+        Container c = this.getContentPane();
+        c.requestFocus();
+
+        while (true){
+            for(int iY=0 ; iY<BYSize; ++iY){
+                for(int iX=0; iX<BXSize; ++iX){
+                    switch (Map[iY][iX]){
+                        case '#':
+                            aImage = this.Wall;
+                            break;
+                        case  '@':
+                            aImage = this.Man;
+                            iXMan = iX;
+                            iYMan = iY;
+                            break;
+                        case '$':
+                            aImage = this.Box;
+                            char[] MapLine = Stage[stage][iY].toCharArray();
+                            if('.' != MapLine[iX]){
+                                bEndGame = false;
+                            }
+                            break;
+                        case '.' :
+                            aImage = this.Dot;
+                            break;
+                        default:
+                            aImage = this.Road;
+                            break;
+                    }
+                    g.drawImage(aImage, iX*IXSize+LThick, iY*IYSize+MYUpsize, this);
+                }
+                System.out.println(Map[iY]); //디버깅 코드
+            }
+            this.setTitle(TITLE + "[Score : " + iScore + "]");
+            TitleLabel.setText("Score : " + iScore);
+            if(bEndGame == true){
+                ++stage;
+                if(Stage.length <= stage){ //게임종료
+                    JOptionPane.showMessageDialog(null, "종료");
+                    System.exit(0);
+                }
+
+                JOptionPane.showMessageDialog(null,"Next Stage");
+                LoadMap();
+                continue;
+            }
+            break;
+        }//while 문 종료
+
+    }
+
+    public void ManMove(int iX, int iY){
+        if('#' != Map[iY][iX]){ // @가 이동할 위치가 벽이 아닐때
+            if('$' == Map[iY][iX]){ //@가 이동할 위치에 박스가 있을때
+
+                //Map[2*iY - iYMan][2*iX -iXMan] : 박스가 이동할 위치
+                if('#' == Map[2*iY - iYMan][2*iX -iXMan]){ //박스가 이동할 위치가 벽이면 아무도 이동하면 안됨
+                    iY = iYMan;
+                    iX = iXMan;
+                    --iScore;
+                }else if('$' == Map[2*iY - iYMan][2*iX -iXMan]){ //박스가 이동할 위치가 박스면 이동하면 안됨
+                    iY = iYMan;
+                    iX = iXMan;
+                    --iScore;
+                }else { //박스가 이동할 위치가 벽과 박스가 아니면
+                    Map[2*iY - iYMan][2*iX -iXMan] = '$'; //박스를 새로운 위치로 이동
+                }
+            }
+
+            char[] MapLine = Stage[stage][iYMan].toCharArray();
+            if('.' == MapLine[iXMan]){
+                Map[iYMan][iXMan] = '.'; //@의 기존 위치를 '.'으로 만듦
+            }else {
+                Map[iYMan][iXMan] = ' '; //@의 기존 위치를 '길'로 만듦
+            }
+            Map[iY][iX] = '@'; //@를 새로운 위치로 이동시킴
+            ++iScore;
+        }
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
     }
 
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int iX = iXMan; //iX 새 좌표값, iXMan은 기존 좌표값
+        int iY=  iYMan; //iY 새 좌표값, iYMan은 기존 좌표값
+
+        switch (e.getKeyCode()){
+            case KeyEvent.VK_HOME:
+                LoadMap();
+                repaint();
+                return;
+            case KeyEvent.VK_UP:
+                Man = ManB;
+                --iY;
+                break;
+            case KeyEvent.VK_DOWN:
+                Man = ManF;
+                ++iY;
+                break;
+            case KeyEvent.VK_LEFT:
+                Man = ManL;
+                --iX;
+                break;
+            case KeyEvent.VK_RIGHT:
+                Man = ManR;
+                ++iX;
+                break;
+            default:
+                return;
+
+        }
+        ManMove(iX,iY);
+        repaint();
+
+    }
+
     @Override
     public void keyTyped(KeyEvent e) {}
-
-    @Override
-    public void keyPressed(KeyEvent e) {}
-
     @Override
     public void keyReleased(KeyEvent e) {}
 }
